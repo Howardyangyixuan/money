ebf999d78a8afebf10ca88c1b0992b3ceda9e05a
howardyangyixuan <howardyangyixuan@gmail.com>
2020-07-21 09:35:54 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div> 404！当前页面不存在</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+<!--    <a href="#/">返回首页</a>-->
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
