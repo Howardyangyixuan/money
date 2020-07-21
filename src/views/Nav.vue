ff51a9206b45b8052131300d402f3cbce5aa6e79
howardyangyixuan <howardyangyixuan@gmail.com>
2020-07-21 23:00:26 +0800

    完成Icon封装，并使用

--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="nav">
     <router-link to="/money">记账</router-link>
-    <svg>
-      <use xlink:href="#money"/>
-    </svg>
+    <Icon name="money"/>
     <router-link to="/labels">标签</router-link>
-    <svg>
-      <use xlink:href="#labels"/>
-    </svg>
+    <Icon name="labels"/>
     <router-link to="/statistics">统计</router-link>
-    <svg>
-      <use xlink:href="#statistics"/>
-    </svg>
-
+    <Icon name="statistics"/>
   </div>
 </template>
 
 <script lang="ts">
-  const importAll = (requireContext: __WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext)
-  try { importAll(require.context('../assets/icons',true,/\.svg$/)) }catch(error){ console.log(error) }
+  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try { importAll(require.context('../assets/icons', true, /\.svg$/)); } catch (error) { console.log(error); }
   // import money from '@/assets/icons/money.svg';
   // import labels from '@/assets/icons/labels.svg';
 
