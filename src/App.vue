b06ed6bc45fdbe2570416f85c0a75d59f1257063
howardyangyixuan <howardyangyixuan@gmail.com>
2020-07-30 16:20:26 +0800

    完成Labels的html和css

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 <style lang="scss">
   @import "~@/assets/style/helper.scss";
   @import "~@/assets/style/reset.scss";
+
   #app {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
@@ -14,5 +15,6 @@
     color: #333;
     font-family: $font-hei;
     line-height: 1.5;
+    background: #f5f5f5;
   }
 </style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,26 @@
 <template>
   <Layout>
-    Labels.vue
+    <ol class="tags">
+      <li><span>衣 </span>
+        <Icon name="right"/>
+      </li>
+      <li><span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li><span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li><span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">
+        新建标签
+      </button>
+    </div>
   </Layout>
+
 </template>
 
 <script lang="ts">
@@ -11,5 +30,36 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
 
+  .tags {
+    background: white;
+
+    > li {
+      padding: 0 6px;
+      @extend %outerShadow;
+      min-height: 44px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      svg {
+        color: #666;
+        width: 18px;
+        height: 18px;
+      }
+    }
+  }
+
+  .createTag {
+    font-size: 16px;
+    background: grey;
+    color: white;
+    border-radius: 4px;
+    border: none;
+    padding: 5px 10px;
+    &-wrapper{
+      padding: 15px;
+    }
+  }
 </style>
