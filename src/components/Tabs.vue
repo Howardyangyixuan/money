aed403e3253acf5977922bc02ec2469c517513b1
howardyangyixuan <howardyangyixuan@gmail.com>
2020-08-06 11:37:32 +0800

    封装Tabs，变成通用的标签栏，替代Types

---
+++ src/components/Tabs.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <ul class="tabs">
+      <li @click="select(item)" v-for=
+        "item in dataSource" :key="item.value"
+          :class="{[classPrefix+'-item']:classPrefix,selected:value === item.value}">
+        {{item.text}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  type dataSourceItem = {
+    text: string;
+    value: string;
+  }
+
+  @Component
+  export default class Tabs extends Vue {
+    @Prop() dataSource!: dataSourceItem[];
+    @Prop(String) readonly value: string | undefined;
+    @Prop(String) classPrefix?: string;
+
+    select(item: dataSourceItem) {
+      let idx = -1;
+      for (let i = 0; i < this.dataSource.length; i++) {
+        if (item.value === this.dataSource[i].value) {
+          idx = i;
+          break;
+        }
+      }
+      if (idx === -1) {
+        throw new Error('value is unknown');
+      }
+      this.$emit('update:value', item.value);
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tabs {
+    background: #c4c4c4;
+    display: flex;
+    font-size: 3.5vh;
+
+    > li {
+      width: 50%;
+      height: 10vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
-    <!--    <Types :value.sync="record.type"/>-->
-    <Types class-prefix="type" :value.sync="record.type"/>
+    <Tabs class-prefix="type" :data-source="types" :value.sync="record.type"/>
+    <Tabs class-prefix="interval" :data-source="intervals" :value.sync="interval.value"/>
   </Layout>
 </template>
 
@@ -9,18 +9,22 @@
   import Types from '@/components/Money/Types.vue';
   import {Component} from 'vue-property-decorator';
   import Vue from 'vue';
+  import Tabs from '@/components/Tabs.vue';
 
   @Component({
-    components: {Types},
+    components: {Tabs, Types},
   })
   export default class extends Vue {
     name = 'Statistics';
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
+    interval = {text: '按天', value: 'day'};
+    types = [{text: '支出', value: '-'}, {text: '收入', value: '+'}];
+    intervals = [{text: '按天', value: 'day'}, {text: '按月', value: 'mouth'}, {text: '按年', value: 'year'}];
   }
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep .type-item {
+  ::v-deep .type-item, {
     background: white;
 
     &.selected {
