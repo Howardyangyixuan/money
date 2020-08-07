1bd5d2d269ee773461f00b00719ede721e287e1e
howardyangyixuan <howardyangyixuan@gmail.com>
2020-08-07 09:35:57 +0800

    deploy

---
+++ precache-manifest.d3dd5733d5492084a38b3af76358240f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c182b2389fbbc43c6ff",
+    "url": "/money/css/app.e9744487.css"
+  },
+  {
+    "revision": "e3db6666c2fd62e6761aba9f335ec3e6",
+    "url": "/money/index.html"
+  },
+  {
+    "revision": "5c182b2389fbbc43c6ff",
+    "url": "/money/js/app.fa044971.js"
+  },
+  {
+    "revision": "ae5258538dc0a8dc14d9",
+    "url": "/money/js/chunk-vendors.523bb330.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money/robots.txt"
+  }
+]);
